ab1bdb80f2a105afefec4d5c38a6a5d68dbfc4d8
Mark Artishuk <markyshuk@gmail.com>
2020-03-19 12:15:57 -0700

    Finished rock paper scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    if n <= 0:
+        return [[]]
+
+    possibilities = []
+    options = ["rock", "paper", "scissors"]
+
+    def helper(roundElem, roundNumber):
+        for i in range(0, len(options)):
+            roundElem.append(options[i])
+            if roundNumber == n:
+                possibilities.append(roundElem.copy())
+            else:
+                helper(roundElem, roundNumber + 1)
+
+            roundElem.pop()
+
+    # Each element is an array containing n amount of elements
+    helper([], 1)
+    return possibilities
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print("Usage: rps.py [num_plays]")
+
